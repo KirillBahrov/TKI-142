90367d4869d17a05112e7446a27f20abb805c411
KirillBahrov <144620561+KirillBahrov@users.noreply.github.com>
2023-12-28 11:46:30 +0300

    Update 4-2 (2).c

--- 4-2 (2).c
+++ 4-2 (2).c
@@ -40,7 +40,7 @@ random_choice = 2,
 * @param length длина массива
 * @return array указатель на пустой массив
 */
-int* get_array(const size_t length);
+int* get_mem_array(const size_t length);
 
 /**
 * @brief фукнция считает новую длину под масиив из пункта 2
@@ -125,7 +125,7 @@ puts("insert a length of array\n");
 size_t length = get_size_t();
 printf("if you fill array by youself, press %d, if you fill array by random numbers, press %d\n", (enum Choices)(user_array), (enum Choices)(random_array));
 int choice = get_int();
-int* mas = get_array(length);
+int* mas = get_mem_array(length);
 
 switch ((enum Choices)choice)
 {
@@ -144,7 +144,7 @@ switch ((enum Choices)choice)
 puts("Array\n");
 show_array(mas, length);
 
-int* task_1_array = get_array(length);
+int* task_1_array = get_mem_array(length);
 first_task(mas, task_1_array, length);
 puts("First task\n");
 show_array(task_1_array, length);
@@ -158,13 +158,13 @@ puts("Insert a valid number!\n");
 return 1;
 }
 
-int* task_2_1 = get_array(new_length(mas, length, k));
+int* task_2_1 = get_mem_array(new_length(mas, length, k));
 second_task(mas, task_2_1, length, k);
 puts("Second task\n");
 show_array(task_2_1, new_length(mas, length, k));
 free_array(&task_2_1);
 
-int* task_3_array = get_array(length);
+int* task_3_array = get_mem_array(length);
 third_task(mas, task_3_array, length);
 puts("Third task\n");
 show_array(task_3_array, length);
